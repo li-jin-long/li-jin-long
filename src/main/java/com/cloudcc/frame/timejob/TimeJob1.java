7bfe690aa8d3ae53884c89f8aaeeb004f5c7642b
15202845751 <1464949391@qq.com>
2021-06-25 15:32:07 +0800

    初始化

---
+++ src/main/java/com/cloudcc/frame/timejob/TimeJob1.java
@@ -0,0 +1,12 @@
+package com.cloudcc.frame.timejob;
+
+import com.cloudcc.deveditor.core.CCObject;
+import com.cloudcc.deveditor.trigger.TriggerEditor;
+
+public class TimeJob1 extends TriggerEditor {
+    public void test(){
+        String a="123123";
+        String b="123123";
+
+    }
+}
--- src/main/java/com/cloudcc/frame/trigger/Tigger1.java
+++ src/main/java/com/cloudcc/frame/trigger/Tigger1.java
@@ -1,10 +1,24 @@
 package com.cloudcc.frame.trigger;
 
+import com.cloudcc.deveditor.core.CCObject;
+import com.cloudcc.deveditor.core.CCService;
+import com.cloudcc.deveditor.core.ServiceResult;
 import com.cloudcc.deveditor.trigger.TriggerEditor;
+
+import java.util.List;
 
 public class Tigger1  extends TriggerEditor {
 
-    public void test1(){
+    public void 打印用户名字(){
+        CCService ccs = new CCService(userInfo);
+        String money = record_new.get("money") == null ? "" : record_new.get("money") + "";//金额
+        String user = record_new.get("username") == null ? "" : record_new.get("username") + "";//用户
+        List<CCObject> ccuser = ccs.cquery("ccuser", "id='" + user + "'");
+        if(ccuser.size()>0){
+            String name = ccuser.get(0).get("name") == null ? "" : ccuser.get(0).get("name") + "";
+            if(true)trigger.addErrorMessage(name);
 
+        }
     }
+
 }
